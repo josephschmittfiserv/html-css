2979f08f6d25c2021edbbf012417f77b56153792
F93AB7X <joseph.schmitt1@fiserv.com>
2023-12-04 13:53:29 -0500

    flask working

--- todo/app.py
+++ todo/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello world'
+def login():
+    return render_template('login.html')
+
+@app.route('/todo.html')
+def todo():
+    return render_template('todo.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
