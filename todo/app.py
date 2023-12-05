e69cd2d39c0c4e345578ebf9f6455922c30c78e3
F93AB7X <joseph.schmitt1@fiserv.com>
2023-12-05 12:15:41 -0500

    using sqlite through flask to implement login authentication

--- todo/app.py
+++ todo/app.py
@@ -1,15 +1,82 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user
 
 app = Flask(__name__)
 
-@app.route('/')
+# Tells flask-sqlalchemy what database to connect to
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+# Enter a secret key
+app.config["SECRET_KEY"] = "fiserv"
+# Initialize flask-sqlalchemy extension
+db = SQLAlchemy()
+
+# LoginManager is needed for our application 
+# to be able to log in and out users
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Create user model
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True,
+                nullable=False)
+    password = db.Column(db.String(250),
+                nullable=False)
+
+
+# Initialize app with extension
+db.init_app(app)
+# Create database within app context
+
+with app.app_context():
+    db.create_all()
+
+# Creates a user loader callback that returns the user object given an id
+@login_manager.user_loader
+def loader_user(user_id):
+    return Users.query.get(user_id)
+
+
+@app.route('/', methods=["GET", "POST"])
+def createAcc():
+  # If the user made a POST request, create a new user
+    if request.method == "POST":
+        user = Users(username=request.form.get("username"),
+                     password=request.form.get("password"))
+        # Add the user to the database
+        db.session.add(user)
+        # Commit the changes made
+        db.session.commit()
+        # Once user account created, redirect them
+        # to login route (created later on)
+        return redirect(url_for("login"))
+    # Renders sign_up template if user made a GET request
+    return render_template("createacc.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template('login.html')
+    # If a post request was made, find the user by 
+    # filtering for the username
+    if request.method == "POST":
+        user = Users.query.filter_by(
+            username=request.form.get("username")).first()
+        # Check if the password entered is the 
+        # same as the user's password
+        if user.password == request.form.get("password"):
+            # Use the login_user method to log in the user
+            login_user(user)
+            return redirect(url_for("todo"))
+        # Redirect the user back to the home
+        # (we'll create the home route in a moment)
+    return render_template("login.html")
+
 
 @app.route('/todo.html')
 def todo():
     return render_template('todo.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
